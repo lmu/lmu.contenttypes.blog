83d4b8ea9b145c5158299219564bcc53d383b886
Alexander Loechel <Alexander.Loechel@lmu.de>
2015-06-03 01:30:17 +0200

    link to homepage, translation of read comments, temprraory deactivate form

--- src/lmu/contenttypes/blog/content.py
+++ src/lmu/contenttypes/blog/content.py
@@ -1,25 +1,19 @@
 # -*- coding: utf-8 -*-
 
 from Products.CMFPlone.interfaces.syndication import ISyndicatable
-
-from Products.statusmessages.interfaces import IStatusMessage
-
+#from Products.statusmessages.interfaces import IStatusMessage
 #from plone.dexterity.content import Item
 from plone.dexterity.content import Container
-
-#from plone.app.contenttypes.behaviors.leadimage import ILeadImage
 from plone.app.discussion.interfaces import IConversation
-from plone.directives import form
 
 from zope.interface import implements
-
-from z3c.form import button
+#from z3c.form import button
 
 from lmu.contenttypes.blog.interfaces import IBlogFolder
 from lmu.contenttypes.blog.interfaces import IBlogEntry
-from lmu.contenttypes.blog.interfaces import IBlogReportForm
+#from lmu.contenttypes.blog.interfaces import IBlogReportForm
 
-from lmu.contenttypes.blog import MESSAGE_FACTORY as _
+#from lmu.contenttypes.blog import MESSAGE_FACTORY as _
 
 
 class BlogFolder(Container):
@@ -40,48 +34,48 @@ class BlogEntry(Container):
             return conversation.total_comments
 
 
-class BlogReportForm(form.SchemaForm):
-    implements(IBlogReportForm)
+#class BlogReportForm(form.SchemaForm):
+#    implements(IBlogReportForm)
+#
+#    schema = IBlogReportForm
+#    ignoreContext = True
 
-    schema = IBlogReportForm
-    ignoreContext = True
+#    label = _(u"Report a Blog Entry")
+#    description = _(u"Report a Blog Entry that violates the Blog rules.")
 
-    label = _(u"Report a Blog Entry")
-    description = _(u"Report a Blog Entry that violates the Blog rules.")
+#    @button.buttonAndHandler(_(u"Report Blog Entry"), name="report")
+#    def handle_report(self, action):
+#        data, errors = self.extractData()
+#        if errors:
+#            self.status = self.formErrorsMessage
+#            return
 
-    @button.buttonAndHandler(_(u"Report Blog Entry"), name="report")
-    def handle_report(self, action):
-        data, errors = self.extractData()
-        if errors:
-            self.status = self.formErrorsMessage
-            return
+#        # Do something with valid data here
 
-        # Do something with valid data here
+#        # Set status on this form page
+#        # (this status message is not bind to the session
+#        # and does not go thru redirects)
+#        self.status = _(u"Thank you for your Report. E-Mail send to Webmaster.")
+#        IStatusMessage(self.request).addStatusMessage(
+#            _(u"Thank you for your Report. E-Mail send to Webmaster."),
+#            'info')
+#        redirect_url = self.context.absolute_url()
+#        self.request.response.redirect(redirect_url)
 
-        # Set status on this form page
-        # (this status message is not bind to the session
-        # and does not go thru redirects)
-        self.status = _(u"Thank you for your Report. E-Mail send to Webmaster.")
-        IStatusMessage(self.request).addStatusMessage(
-            _(u"Thank you for your Report. E-Mail send to Webmaster."),
-            'info')
-        redirect_url = self.context.absolute_url()
-        self.request.response.redirect(redirect_url)
+#    @button.buttonAndHandler(u"Cancel")
+#    def handle_cancel(self, action):
+#        """User cancelled. Redirect back to the front page.
+#        """
+#        IStatusMessage(self.request).addStatusMessage(
+#            "Notting Reported",
+#            'info')
+#        redirect_url = self.context.absolute_url()
+#        self.request.response.redirect(redirect_url)
 
-    @button.buttonAndHandler(u"Cancel")
-    def handle_cancel(self, action):
-        """User cancelled. Redirect back to the front page.
-        """
-        IStatusMessage(self.request).addStatusMessage(
-            "Notting Reported",
-            'info')
-        redirect_url = self.context.absolute_url()
-        self.request.response.redirect(redirect_url)
-
-    def updateActions(self):
-        super(BlogReportForm, self).updateActions()
-        self.actions['cancel'].addClass("button small")
-        self.actions['report'].addClass("button small")
+#    def updateActions(self):
+#        super(BlogReportForm, self).updateActions()
+#        self.actions['cancel'].addClass("button small")
+#        self.actions['report'].addClass("button small")
 
 #    def updateFields(self):
 #        super(BlogReportForm, self).updateFields()
--- src/lmu/contenttypes/blog/interfaces.py
+++ src/lmu/contenttypes/blog/interfaces.py
@@ -2,10 +2,10 @@
 
 from plone.namedfile.interfaces import IImageScaleTraversable
 from plone.theme.interfaces import IDefaultPloneLayer
-from zope import schema
+#from zope import schema
 from zope.interface import Interface
 
-from lmu.contenttypes.blog import MESSAGE_FACTORY as _
+#from lmu.contenttypes.blog import MESSAGE_FACTORY as _
 
 
 class IBlogFolder(Interface, IImageScaleTraversable):
@@ -28,19 +28,19 @@ class IBlogLayer(IDefaultPloneLayer):
     """
 
 
-class IBlogReportForm(Interface):
+# class IBlogReportForm(Interface):
 
-    name = schema.TextLine(
-        title=_(u"Name of Reporter"),
-        description=_(u"Name of the Person reporting this issue, will be filled by System.")
-    )
+#     name = schema.TextLine(
+#         title=_(u"Name of Reporter"),
+#         description=_(u"Name of the Person reporting this issue, will be filled by System.")
+#     )
 
-    url = schema.URI(
-        title=_(u"Reported URL"),
-        description=_(u"The Reported URL of the Blog Entry.")
-    )
+#     url = schema.URI(
+#         title=_(u"Reported URL"),
+#         description=_(u"The Reported URL of the Blog Entry.")
+#     )
 
-    message = schema.Text(
-        title=_(u"Report Message"),
-        description=_(u"Please describe why you report this Blog Entry")
-    )
+#     message = schema.Text(
+#         title=_(u"Report Message"),
+#         description=_(u"Please describe why you report this Blog Entry")
+#     )
